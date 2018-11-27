1973076a28b080ae4769c077bdae4042bf8955f0
Sabina Dhaugoda <44160277+sabeenski@users.noreply.github.com>
2018-11-27 15:09:19 +0100

    server entity

---
+++ server/src/games/entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity } from 'typeorm/repository/BaseEntity'
+
+@Entity()
+export default class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('text', {nullable:false})
+  word: string
+
+  
+}

5a6277efefb89135a870143520875af550b9acf3
sabeenski <sabinadhaugoda@gmail.com>
2018-11-27 15:07:37 +0100

    server controller

---
+++ server/src/games/controller.ts
@@ -0,0 +1,20 @@
+import { JsonController, Get, Post, HttpCode, Body } from 'routing-controllers'
+import Game from './entity';
+
+@JsonController()
+export default class GameController {
+    @Get('/games')
+        async allGames(){
+            const words = await Game.find()
+            return { words }
+        }
+    
+    @Post('/games')
+        @HttpCode(201)
+        createGame(
+            @Body() game: Game
+        ) {
+            return game.save ()
+        }
+        
+}

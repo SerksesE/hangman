df4ad2e5950ee55e16edf3628ed149f04177d2cb
SerksesE <serkses@hotmail.com>
2018-11-28 11:05:18 +0100

    setup server-client

--- server/src/games/controller.ts
+++ server/src/games/controller.ts
@@ -3,18 +3,18 @@ import Game from './entity';
 
 @JsonController()
 export default class GameController {
-    @Get('/games')
-        async allGames(){
-            const words = await Game.find()
-            return { words }
-        }
-    
-    @Post('/games')
-        @HttpCode(201)
-        createGame(
-            @Body() game: Game
-        ) {
-            return game.save ()
-        }
-        
+  @Get('/games')
+  async allGames() {
+    const words = await Game.find()
+    return { words }
+  }
+
+  @Post('/games')
+  @HttpCode(201)
+  createGame(
+    @Body() game: Game
+  ) {
+    return game.save()
+  }
+
 }

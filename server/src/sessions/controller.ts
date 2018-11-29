6ae941f9aadfaeedb94cbd4d30954ead48539cee
SerksesE <serkses@hotmail.com>
2018-11-29 17:12:18 +0100

    update backend

--- server/src/sessions/controller.ts
+++ server/src/sessions/controller.ts
@@ -1,6 +1,8 @@
-import { JsonController, Post, HttpCode, Body, Get, Param } from 'routing-controllers'
+import { JsonController, Post, HttpCode, Body, Get, Patch, Param, NotFoundError } from 'routing-controllers'
 // import User from '../users/entity';
 import GameSession from './entity';
+import Game from '../games/entity';
+// import Game from '../games/entity';
 
 @JsonController()
 export default class SessionController {
@@ -27,40 +29,79 @@ export default class SessionController {
     return session.save()
   }
 
-  // @Patch('/sessions/:id/join')
-  // async userJoin(
-  //   @Body()
-  //   user: User,
-  //   session: GameSession,
-  //   @Param('id') sessionId: number
-  // ) {
+  @Patch('/sessions/:id/join')
+  async userJoin(
+    @Body()
+      playerId: any,
+    // user: User,
+    // session: GameSession,
+    // game: Game,
+    @Param('id') sessionId: number
+  ) {
 
-  // get session id
-  // get user id from the body
+    let player = parseInt(playerId.playerId)
 
-  // const gameId = session.id
-  // const userId = user.id
-
-  // does session have a player 1? if no userid = player1
-  // return response waiting for players
-
-  // if (session.player1 !== null) {
-  //   return session.player2 === userId
-  // }
-  // return session.player1 === userId
-
-  // if yes userid = player2
-  // make 2 games 
+    console.log(player)
+    
+    const gameSession = await GameSession.findOne(sessionId)
+    if (!gameSession) throw new NotFoundError('Session does not exist')
 
 
 
-  // game 1 belongs to player1
-  // game1.userid = session.player1
+    // get session id
+    // get user id from the body
 
-  // game 2 belongs to player2
-  // game2.userid = session.player2
+    // does session have a player 1? if no userid = player1
+    // return response waiting for players
 
-  // return a response with start with oppenents game id and your own 
+    console.log(gameSession)
 
-  // }
+    if (gameSession.player1 === null) {
+
+      console.log('hello')
+      gameSession.player1 = player
+
+      console.log(gameSession)
+      return gameSession.save()  
+    } else if (gameSession.player1 !== null) {
+      gameSession.player2 = player
+    }
+    console.log(gameSession)
+
+    const game1 = await Game.create({
+      userId: gameSession.player1
+    }).save()
+    console.log(game1)
+    const game2 = await Game.create({
+      userId: gameSession.player2
+    }).save()
+    console.log(game2)
+
+    if(game1.id === undefined) throw Error
+    gameSession.game1 = game1.id 
+
+    if(game2.id === undefined) throw Error
+    gameSession.game2 = game2.id 
+
+    // if yes userid = player2
+    // make 2 games 
+
+    // if (session.player1 && session.player2 !== null) {
+
+    // }
+    // return game.save()
+
+    // game 1 belongs to player1
+    // game1.userid = session.player1
+
+
+
+    // game 2 belongs to player2
+    // game2.userid = session.player2
+
+    // return a response with start with oppenents game id and your own
+
+    console.log(gameSession) 
+    return gameSession.save() 
+  }
 }
